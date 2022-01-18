e98b47ededa48a66f634d24ab04c997697af38fe
Vladimir Shefer <quameu@gmail.com>
2022-01-19 01:29:58 +0300

    Fix Retention for annotations

--- spring-boot-starter-telegram/src/main/java/com/github/vladimirshefer/springbootstartertelegram/annotations/MessageBody.java
+++ spring-boot-starter-telegram/src/main/java/com/github/vladimirshefer/springbootstartertelegram/annotations/MessageBody.java
@@ -1,6 +1,8 @@
 package com.github.vladimirshefer.springbootstartertelegram.annotations;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
@@ -8,6 +10,7 @@ import java.lang.annotation.Target;
  * Same as @RequestBody in Spring web.
  */
 @Target({ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface MessageBody {
 
 }
