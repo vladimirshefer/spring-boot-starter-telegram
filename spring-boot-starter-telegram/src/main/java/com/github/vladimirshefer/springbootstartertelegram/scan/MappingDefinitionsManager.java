2f72191af56684c8be28a3673fa9a835a4e4b576
dkurnikov <kurnikov.dmitrii@gmail.com>
2022-01-20 20:42:27 +0300

    Add comments

--- spring-boot-starter-telegram/src/main/java/com/github/vladimirshefer/springbootstartertelegram/scan/MappingDefinitionsManager.java
+++ spring-boot-starter-telegram/src/main/java/com/github/vladimirshefer/springbootstartertelegram/scan/MappingDefinitionsManager.java
@@ -8,7 +8,10 @@ import org.telegram.telegrambots.meta.api.objects.PhotoSize;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.polls.Poll;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -20,17 +23,18 @@ public class MappingDefinitionsManager {
   public void registerController(String controllerName, Class<?> controllerClass,
                                  Object controllerBean) {
     List<MappingDefinition> mappingDefinitionsForController = getMappingDefinitionsForController(
-            controllerName, controllerClass, controllerBean);
+      controllerName, controllerClass, controllerBean);
     mappingDefinitions.addAll(mappingDefinitionsForController);
   }
 
   public Optional<MappingDefinition> findMappingDefinition(Update update) {
     String text = UpdateUtil.getMessageTextOrNull(update);
 
-
-    if (text != null){
+/*если у нас нет никаких параметров у аннотации мы идем искать методы в которые мым можем положить
+   значения по сигнатуре  */
+    if (text != null) {
       return findDefinitionForText(text, update);
-    }else{
+    } else {
       return findDefinitionForDefaultMapping(update);
     }
 
@@ -40,20 +44,21 @@ public class MappingDefinitionsManager {
     Stream<MappingDefinition> mappingDefinitionStream = mappingDefinitions.stream()
       .filter(it -> it.getMappingAnnotation().regex().equals(""))
       .filter(it -> it.getMappingAnnotation().value().equals(""));
-
-    if(UpdateUtil.getPollOrNull(update) != null){
+    /* и тут проверять если необходимые параметры в методе */
+    if (UpdateUtil.getPollOrNull(update) != null) {
       mappingDefinitionStream = mappingDefinitionStream.filter(method ->
         Arrays.asList(method.getTargetMethod().getParameterTypes()).contains(Poll.class));
     }
-    if(UpdateUtil.getPhotoOrNull(update) != null){
+    if (UpdateUtil.getPhotoOrNull(update) != null) {
       mappingDefinitionStream = mappingDefinitionStream.filter(method ->
         Arrays.asList(method.getTargetMethod().getParameterTypes()).contains(PhotoSize.class));
     }
 
     List<MappingDefinition> listOfMappingDefinitions = mappingDefinitionStream.collect(Collectors.toList());
-
-    if (listOfMappingDefinitions.size() == 1){
-       return Optional.ofNullable(listOfMappingDefinitions.get(0));
+/*надо еще как до добаить типа если ничего не нашли то пытаться искать методы с параметрами Update или
+ Message или метод с пустой сигнатурой*/
+    if (listOfMappingDefinitions.size() == 1) {
+      return Optional.ofNullable(listOfMappingDefinitions.get(0));
     }
     return Optional.empty();
   }
@@ -77,28 +82,28 @@ public class MappingDefinitionsManager {
 
   private Optional<MappingDefinition> findDefinitionByRegex(String text) {
     return mappingDefinitions.stream()
-            .filter(it -> !"".equals(it.getMappingAnnotation().regex()))
-            .filter(it -> text.matches(it.getMappingAnnotation().regex()))
-            .findAny();
+      .filter(it -> !"".equals(it.getMappingAnnotation().regex()))
+      .filter(it -> text.matches(it.getMappingAnnotation().regex()))
+      .findAny();
   }
 
   private Optional<MappingDefinition> findDefinitionByValue(String text) {
     return mappingDefinitions.stream()
-            .filter(it -> !"".equals(it.getMappingAnnotation().value()))
-            .filter(it -> text.startsWith(it.getMappingAnnotation().value()))
-            .findAny();
+      .filter(it -> !"".equals(it.getMappingAnnotation().value()))
+      .filter(it -> text.startsWith(it.getMappingAnnotation().value()))
+      .findAny();
   }
 
 
   private List<MappingDefinition> getMappingDefinitionsForController(
-          String controllerName,
-          Class<?> controllerClass,
-          Object controllerBean
+    String controllerName,
+    Class<?> controllerClass,
+    Object controllerBean
   ) {
     return Arrays.stream(controllerClass.getDeclaredMethods())
-            .filter(it -> it.getAnnotation(RequestMapping.class) != null)
-            .map(it -> MappingDefinition.of(controllerName, controllerClass, controllerBean, it))
-            .collect(Collectors.toList());
+      .filter(it -> it.getAnnotation(RequestMapping.class) != null)
+      .map(it -> MappingDefinition.of(controllerName, controllerClass, controllerBean, it))
+      .collect(Collectors.toList());
   }
 
 }
