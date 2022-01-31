bbc25de69c1d1780593b300974ce36cd660838a9
Vladimir Shefer <quameu@gmail.com>
2022-02-01 02:56:32 +0300

    Extract ArgumentResolver-s

---
+++ spring-boot-starter-telegram/src/main/java/com/github/vladimirshefer/springbootstartertelegram/argument_resolvers/PollArgumentResolver.java
@@ -0,0 +1,28 @@
+package com.github.vladimirshefer.springbootstartertelegram.argument_resolvers;
+
+import static com.github.vladimirshefer.springbootstartertelegram.telegram.util.UpdateUtil.getPollOrNull;
+
+import com.github.vladimirshefer.springbootstartertelegram.telegram.dto.MappingDefinition;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.polls.Poll;
+
+@Component
+public class PollArgumentResolver implements ArgumentResolver {
+
+  public void resolve(
+    MappingDefinition mappingDefinition,
+    Update update,
+    Object[] result,
+    int index
+  ) {
+    Class<?>[] parameterTypes = mappingDefinition.getOriginalMethod().getParameterTypes();
+    if (parameterTypes[index].equals(Poll.class)) {
+      Poll poll = getPollOrNull(update);
+      if (poll != null) {
+        result[index] = poll;
+      }
+    }
+  }
+
+}
--- spring-boot-starter-telegram/src/main/java/com/github/vladimirshefer/springbootstartertelegram/handler/ControllerInvocationArgumentsResolver.java
+++ spring-boot-starter-telegram/src/main/java/com/github/vladimirshefer/springbootstartertelegram/handler/ControllerInvocationArgumentsResolver.java
@@ -2,40 +2,42 @@ package com.github.vladimirshefer.springbootstartertelegram.handler;
 
 import static com.github.vladimirshefer.springbootstartertelegram.telegram.util.UpdateUtil.getMessageTextOrNull;
 import static com.github.vladimirshefer.springbootstartertelegram.telegram.util.UpdateUtil.getPhotoOrNull;
-import static com.github.vladimirshefer.springbootstartertelegram.telegram.util.UpdateUtil.getPollOrNull;
 
 import com.github.vladimirshefer.springbootstartertelegram.annotations.MessageBody;
+import com.github.vladimirshefer.springbootstartertelegram.argument_resolvers.ArgumentResolver;
 import com.github.vladimirshefer.springbootstartertelegram.telegram.dto.MappingDefinition;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.api.objects.polls.Poll;
 
 @Component
+@RequiredArgsConstructor
 public class ControllerInvocationArgumentsResolver {
 
-  public Object[] getArguments(MappingDefinition mappingDefinition,
-    Update update){
+  private final List<ArgumentResolver> argumentResolvers;
+
+  public Object[] getArguments(
+    MappingDefinition mappingDefinition,
+    Update update
+  ) {
     int parameterCount = mappingDefinition.getOriginalMethod().getParameterCount();
     Object[] parametersArray = new Object[parameterCount];
 
-    Class<?>[] parameterTypes = mappingDefinition.getOriginalMethod().getParameterTypes();
-
-    for (int i = 0; i < parameterTypes.length; i++) {
-      if (parameterTypes[i].equals(Update.class)) {
-        parametersArray[i] = update;
-      }
-      if (parameterTypes[i].equals(Poll.class)){
-        parametersArray[i] = getPollOrNull(update);
-      }
+    int parametersAmount = mappingDefinition.getOriginalMethod().getParameters().length;
+    for (int i = 0; i < parametersAmount; i++) {
+      int index = i;
+      argumentResolvers.forEach(
+        it -> it.resolve(mappingDefinition, update, parametersArray, index));
     }
 
     // I think it`s not good-looking, maybe I do with it
     Type[] genericParameterTypes = mappingDefinition.getOriginalMethod().getGenericParameterTypes();
-    for (int i1 = 0; i1 < genericParameterTypes.length; i1++) {
-      if (genericParameterTypes[i1].getTypeName().contains("PhotoSize")){
-        parametersArray[i1] = getPhotoOrNull(update);
+    for (int i = 0; i < genericParameterTypes.length; i++) {
+      if (genericParameterTypes[i].getTypeName().contains("PhotoSize")) {
+        parametersArray[i] = getPhotoOrNull(update);
       }
     }
 
