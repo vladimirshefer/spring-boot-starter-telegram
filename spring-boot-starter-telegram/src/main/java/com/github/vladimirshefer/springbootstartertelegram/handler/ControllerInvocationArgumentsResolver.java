35128b219eb08b9aeed4c24f6e04d712f4ae5560
Vladimir Shefer <quameu@gmail.com>
2022-02-01 03:05:09 +0300

    Extract PhotoSizeArgumentResolverImpl

--- spring-boot-starter-telegram/src/main/java/com/github/vladimirshefer/springbootstartertelegram/handler/ControllerInvocationArgumentsResolver.java
+++ spring-boot-starter-telegram/src/main/java/com/github/vladimirshefer/springbootstartertelegram/handler/ControllerInvocationArgumentsResolver.java
@@ -1,10 +1,7 @@
 package com.github.vladimirshefer.springbootstartertelegram.handler;
 
-import static com.github.vladimirshefer.springbootstartertelegram.telegram.util.UpdateUtil.getPhotoOrNull;
-
 import com.github.vladimirshefer.springbootstartertelegram.argument_resolvers.ArgumentResolver;
 import com.github.vladimirshefer.springbootstartertelegram.telegram.dto.MappingDefinition;
-import java.lang.reflect.Type;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -24,18 +21,11 @@ public class ControllerInvocationArgumentsResolver {
     Object[] parametersArray = new Object[parameterCount];
 
     int parametersAmount = mappingDefinition.getOriginalMethod().getParameters().length;
+
     for (int i = 0; i < parametersAmount; i++) {
       int index = i;
       argumentResolvers.forEach(
         it -> it.resolve(mappingDefinition, update, parametersArray, index));
-    }
-
-    // I think it`s not good-looking, maybe I do with it
-    Type[] genericParameterTypes = mappingDefinition.getOriginalMethod().getGenericParameterTypes();
-    for (int i = 0; i < genericParameterTypes.length; i++) {
-      if (genericParameterTypes[i].getTypeName().contains("PhotoSize")) {
-        parametersArray[i] = getPhotoOrNull(update);
-      }
     }
 
     return parametersArray;
