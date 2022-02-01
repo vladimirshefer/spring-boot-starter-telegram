a325967c7aa99485a4ca5769c803fb4dcf3557e2
Vladimir Shefer <quameu@gmail.com>
2022-02-01 19:12:36 +0300

    Update demo controller

--- demo-application/src/main/java/com/github/vladimirshefer/demobot/controller/StartController.java
+++ demo-application/src/main/java/com/github/vladimirshefer/demobot/controller/StartController.java
@@ -1,38 +1,51 @@
 package com.github.vladimirshefer.demobot.controller;
 
-import com.github.vladimirshefer.springbootstartertelegram.annotations.MessageBody;
 import com.github.vladimirshefer.springbootstartertelegram.annotations.RequestMapping;
 import com.github.vladimirshefer.springbootstartertelegram.annotations.TelegramController;
+import java.util.List;
 import org.telegram.telegrambots.meta.api.objects.PhotoSize;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.polls.Poll;
 
-import java.util.List;
-
 @TelegramController
 public class StartController {
 
-  @RequestMapping("/start")
-  public String start(@MessageBody String body, Update update) {
-    return "Hello, world!";
+  /**
+   * Simple handler to reply the text messages.
+   *
+   * @param messageText the text of the message, if present.
+   * @return Reply message text to be sent by bot.
+   */
+  @RequestMapping
+  public String simpleMessage(String messageText) {
+    return "Thank you for your message!";
   }
 
   @RequestMapping(regex = "[a-z]{2}")
-  public String blabla(@MessageBody String body, Update update){
+  public String regex(String body, Update update) {
     return body;
   }
 
+  /**
+   * Handles polls created in chat. If message contains no poll, then this handler will not be
+   * invoked.
+   *
+   * @param poll the poll, if present.
+   * @return The reply message, saying the number of options in the poll.
+   */
   @RequestMapping
-  public String pollCheck(Poll poll){
-    return "done!";
-  }
-  @RequestMapping
-  public String pollCheck1(Poll poll){
-    return "done!";
+  public String pollCheck(Poll poll) {
+    return "The poll has " + poll.getOptions().size() + " options";
   }
 
+  /**
+   * Handles messages with photos attached.
+   * @param photos The photos info.
+   * @param caption The string parameter is populated with photos description.
+   * @return
+   */
   @RequestMapping
-  public List<PhotoSize> sentPhoto(List<PhotoSize> photo){
-    return photo;
+  public void photos(List<PhotoSize> photos, String caption) {
   }
+
 }
