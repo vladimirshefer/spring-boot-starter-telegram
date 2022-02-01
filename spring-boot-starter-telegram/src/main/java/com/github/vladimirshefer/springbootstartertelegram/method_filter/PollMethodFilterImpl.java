8a6e1a3d9da288f3905317926b66d222b0fdcba9
dkurnikov <kurnikov.dmitrii@gmail.com>
2022-02-01 16:42:40 +0300

    Implement MethodFilter with PhotoMethodFilterImpl, PollMethodFilterImpl, TextBodyMethodFilterImpl, UpdateMethodFilterImpl

---
+++ spring-boot-starter-telegram/src/main/java/com/github/vladimirshefer/springbootstartertelegram/method_filter/PollMethodFilterImpl.java
@@ -0,0 +1,22 @@
+package com.github.vladimirshefer.springbootstartertelegram.method_filter;
+
+import com.github.vladimirshefer.springbootstartertelegram.telegram.dto.MappingDefinition;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.polls.Poll;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+@Component
+public class PollMethodFilterImpl implements MethodFilter {
+  @Override
+  public boolean isMatch(Update update, MappingDefinition method) {
+    return update.getMessage().getPoll() != null &&
+      Arrays
+        .stream(method.getTargetMethod().getParameterTypes())
+        .collect(Collectors.toList())
+        .contains(Poll.class);
+
+  }
+}
