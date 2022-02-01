34cfbd84dafe2bf2daa19365855129975d194607
Vladimir Shefer <quameu@gmail.com>
2022-02-01 03:01:36 +0300

    Fix naming in argument resolvers impls

--- spring-boot-starter-telegram/src/main/java/com/github/vladimirshefer/springbootstartertelegram/argument_resolvers/PollArgumentResolverImpl.java
+++ spring-boot-starter-telegram/src/main/java/com/github/vladimirshefer/springbootstartertelegram/argument_resolvers/PollArgumentResolverImpl.java
@@ -1,11 +1,14 @@
 package com.github.vladimirshefer.springbootstartertelegram.argument_resolvers;
 
+import static com.github.vladimirshefer.springbootstartertelegram.telegram.util.UpdateUtil.getPollOrNull;
+
 import com.github.vladimirshefer.springbootstartertelegram.telegram.dto.MappingDefinition;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.polls.Poll;
 
 @Component
-public class UpdateArgumentResolver implements ArgumentResolver {
+public class PollArgumentResolverImpl implements ArgumentResolver {
 
   public void resolve(
     MappingDefinition mappingDefinition,
@@ -14,8 +17,11 @@ public class UpdateArgumentResolver implements ArgumentResolver {
     int index
   ) {
     Class<?>[] parameterTypes = mappingDefinition.getOriginalMethod().getParameterTypes();
-    if (parameterTypes[index].equals(Update.class)) {
-      result[index] = update;
+    if (parameterTypes[index].equals(Poll.class)) {
+      Poll poll = getPollOrNull(update);
+      if (poll != null) {
+        result[index] = poll;
+      }
     }
   }
 
--- spring-boot-starter-telegram/src/main/java/com/github/vladimirshefer/springbootstartertelegram/argument_resolvers/UpdateArgumentResolverImpl.java
+++ spring-boot-starter-telegram/src/main/java/com/github/vladimirshefer/springbootstartertelegram/argument_resolvers/UpdateArgumentResolverImpl.java
@@ -1,14 +1,11 @@
 package com.github.vladimirshefer.springbootstartertelegram.argument_resolvers;
 
-import static com.github.vladimirshefer.springbootstartertelegram.telegram.util.UpdateUtil.getPollOrNull;
-
 import com.github.vladimirshefer.springbootstartertelegram.telegram.dto.MappingDefinition;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.api.objects.polls.Poll;
 
 @Component
-public class PollArgumentResolver implements ArgumentResolver {
+public class UpdateArgumentResolverImpl implements ArgumentResolver {
 
   public void resolve(
     MappingDefinition mappingDefinition,
@@ -17,11 +14,8 @@ public class PollArgumentResolver implements ArgumentResolver {
     int index
   ) {
     Class<?>[] parameterTypes = mappingDefinition.getOriginalMethod().getParameterTypes();
-    if (parameterTypes[index].equals(Poll.class)) {
-      Poll poll = getPollOrNull(update);
-      if (poll != null) {
-        result[index] = poll;
-      }
+    if (parameterTypes[index].equals(Update.class)) {
+      result[index] = update;
     }
   }
 
