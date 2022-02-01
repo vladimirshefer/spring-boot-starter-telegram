3751aac79a9988a2e26312339f4068a452a458e7
Vladimir Shefer <quameu@gmail.com>
2022-02-01 18:28:55 +0300

    Remove @Enable... annotation and add spring.factories
    This change will enable telegram controllers without any extra code

--- demo-application/src/main/java/com/github/vladimirshefer/demobot/SpringBootStarterTelegramApplication.java
+++ demo-application/src/main/java/com/github/vladimirshefer/demobot/SpringBootStarterTelegramApplication.java
@@ -1,10 +1,8 @@
 package com.github.vladimirshefer.demobot;
 
-import com.github.vladimirshefer.springbootstartertelegram.annotations.EnableTelegramBots;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableTelegramBots
 @SpringBootApplication
 public class SpringBootStarterTelegramApplication {
 
