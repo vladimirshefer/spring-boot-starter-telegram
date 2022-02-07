b8aff30922cb56b4791cf9785150aa92c0d10b36
Vladimir Shefer <quameu@gmail.com>
2022-02-07 23:32:41 +0300

    Refactoring in pattern matcher method filter

---
+++ spring-boot-starter-telegram/src/main/java/io/github/vladimirshefer/springbootstartertelegram/method_filter/RequestMappingAnnotationPatternMatchesMethodFilterImpl.java
@@ -0,0 +1,22 @@
+package io.github.vladimirshefer.springbootstartertelegram.method_filter;
+
+import io.github.vladimirshefer.springbootstartertelegram.telegram.dto.MappingDefinition;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Component
+public class RequestMappingAnnotationPatternMatchesMethodFilterImpl implements MethodFilter {
+
+  @Override
+  public boolean isMatch(Update update, MappingDefinition method) {
+    boolean noMapping = method.getRequestMappingValue().equals("");
+
+    return noMapping || textMatchess(update, method);
+  }
+
+  private boolean textMatchess(Update update, MappingDefinition method) {
+    String text = update.getMessage().getText();
+    return text != null && text.matches(method.getRequestMappingValue());
+  }
+
+}
