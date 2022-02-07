50b88aee1bb471d3a1a357ea74482b54918b0346
Vladimir Shefer <quameu@gmail.com>
2022-02-07 17:53:15 +0300

    Rename annotation @MessageBody to @MessageText

--- spring-boot-starter-telegram/src/main/java/io/github/vladimirshefer/springbootstartertelegram/annotations/MessageText.java
+++ spring-boot-starter-telegram/src/main/java/io/github/vladimirshefer/springbootstartertelegram/annotations/MessageText.java
@@ -7,10 +7,14 @@ import java.lang.annotation.Target;
 
 /**
  * Puts the contents of the message text to the parameter value.
+ *
+ * If the annotation is combined with @Nullable annotation, then this parameter is considered as optional.
+ * This means that handler method will be invoked, but the value of parameter will be null if no message text present in Update.
+ *
  * Same as @RequestBody in Spring web.
  */
 @Target({ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
-public @interface MessageBody {
+public @interface MessageText {
 
 }
--- spring-boot-starter-telegram/src/main/java/io/github/vladimirshefer/springbootstartertelegram/argument_resolvers/MessageTextAnnotationArgumentResolverImpl.java
+++ spring-boot-starter-telegram/src/main/java/io/github/vladimirshefer/springbootstartertelegram/argument_resolvers/MessageTextAnnotationArgumentResolverImpl.java
@@ -2,14 +2,14 @@ package io.github.vladimirshefer.springbootstartertelegram.argument_resolvers;
 
 import static io.github.vladimirshefer.springbootstartertelegram.telegram.util.UpdateUtil.getMessageTextOrNull;
 
-import io.github.vladimirshefer.springbootstartertelegram.annotations.MessageBody;
+import io.github.vladimirshefer.springbootstartertelegram.annotations.MessageText;
 import io.github.vladimirshefer.springbootstartertelegram.telegram.dto.MappingDefinition;
 import java.lang.annotation.Annotation;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 @Component
-public class MessageBodyAnnotationArgumentResolverImpl implements ArgumentResolver {
+public class MessageTextAnnotationArgumentResolverImpl implements ArgumentResolver {
 
   @Override
   public void resolve(
@@ -23,7 +23,7 @@ public class MessageBodyAnnotationArgumentResolverImpl implements ArgumentResolv
       .getParameterAnnotations()[index];
 
     for (Annotation parameterAnnotation : parameterAnnotations) {
-      if (parameterAnnotation.annotationType().equals(MessageBody.class)) {
+      if (parameterAnnotation.annotationType().equals(MessageText.class)) {
         result[index] = getMessageTextOrNull(update);
       }
     }
