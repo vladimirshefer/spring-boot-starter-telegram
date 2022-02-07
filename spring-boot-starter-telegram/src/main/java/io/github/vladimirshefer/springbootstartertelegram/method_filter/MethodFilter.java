071ed2ae4586b0ca9d52113540c7911c72214c88
Vladimir Shefer <quameu@gmail.com>
2022-02-07 14:56:57 +0300

    Add javadoc for MethodFilter

--- spring-boot-starter-telegram/src/main/java/io/github/vladimirshefer/springbootstartertelegram/method_filter/MethodFilter.java
+++ spring-boot-starter-telegram/src/main/java/io/github/vladimirshefer/springbootstartertelegram/method_filter/MethodFilter.java
@@ -3,8 +3,17 @@ package io.github.vladimirshefer.springbootstartertelegram.method_filter;
 import io.github.vladimirshefer.springbootstartertelegram.telegram.dto.MappingDefinition;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
+/**
+ * Gives ability to filter out controller methods if they are not appropriate for specific telegram
+ * message.
+ */
 public interface MethodFilter {
 
+  /**
+   * @param update the telegram message
+   * @param method
+   * @return false if this update should not be handled by this method. True if you don't mind.
+   */
   boolean isMatch(Update update, MappingDefinition method);
 
 }
