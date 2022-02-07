75daf000bbd284bb29d3c992fdab599f013ad004
Vladimir Shefer <quameu@gmail.com>
2022-02-07 23:24:51 +0300

    ArgumentResolver: Drop deprecated method

--- spring-boot-starter-telegram/src/main/java/io/github/vladimirshefer/springbootstartertelegram/argument_resolvers/ArgumentResolver.java
+++ spring-boot-starter-telegram/src/main/java/io/github/vladimirshefer/springbootstartertelegram/argument_resolvers/ArgumentResolver.java
@@ -6,32 +6,18 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 
 public interface ArgumentResolver {
 
-  @Deprecated
-  default void resolve(
-    MappingDefinition mappingDefinition,
-    Update update,
-    Object[] result,
-    int index
-  ) {
-    result[index] = this.resolve(mappingDefinition, update, index);
-  }
-
   /**
    * Retrieves required information for the handler parameter from the update.
    * @param mappingDefinition The full information about handler method.
    * @param update The incoming update (i.e. telegram message)
    * @param index The index of the handler method parameter to resolve.
-   * @return
+   * @return the value for argument. null if this handler should not or could not set the value.
    */
   @Nullable
-  default Object resolve(
+  Object resolve(
     MappingDefinition mappingDefinition,
     Update update,
     int index
-  ){
-    Object[] result = new Object[index + 1];
-    this.resolve(mappingDefinition, update, result, index);
-    return result[index];
-  }
+  );
 
 }
