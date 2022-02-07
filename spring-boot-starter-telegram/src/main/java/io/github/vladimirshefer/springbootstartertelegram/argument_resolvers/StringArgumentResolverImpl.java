c158dc5d09b32024578ba0ffbec35ca6244cda8d
Vladimir Shefer <quameu@gmail.com>
2022-02-08 00:41:39 +0300

    Refactoring: StringArgumentResolverImpl

--- spring-boot-starter-telegram/src/main/java/io/github/vladimirshefer/springbootstartertelegram/argument_resolvers/StringArgumentResolverImpl.java
+++ spring-boot-starter-telegram/src/main/java/io/github/vladimirshefer/springbootstartertelegram/argument_resolvers/StringArgumentResolverImpl.java
@@ -7,11 +7,13 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 
 @Component
 public class StringArgumentResolverImpl implements ArgumentResolver {
+
   @Override
   public Object resolve(
     MappingDefinition mappingDefinition,
     Update update,
-    int index) {
+    int index
+  ) {
     Class<?>[] parameterTypes = mappingDefinition.getOriginalMethod().getParameterTypes();
     if (parameterTypes[index].equals(String.class)) {
       return UpdateUtil.getMessageTextOrNull(update);
