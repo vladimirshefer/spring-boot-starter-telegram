76af26dd09dcafebf8e8b0bd1b626c69baf9f418
Vladimir Shefer <quameu@gmail.com>
2022-02-07 18:04:17 +0300

    ArgumentResolver: Add Javadoc

--- spring-boot-starter-telegram/src/main/java/io/github/vladimirshefer/springbootstartertelegram/argument_resolvers/ArgumentResolver.java
+++ spring-boot-starter-telegram/src/main/java/io/github/vladimirshefer/springbootstartertelegram/argument_resolvers/ArgumentResolver.java
@@ -1,10 +1,12 @@
 package io.github.vladimirshefer.springbootstartertelegram.argument_resolvers;
 
 import io.github.vladimirshefer.springbootstartertelegram.telegram.dto.MappingDefinition;
+import javax.annotation.Nullable;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 public interface ArgumentResolver {
 
+  @Deprecated
   default void resolve(
     MappingDefinition mappingDefinition,
     Update update,
@@ -14,6 +16,14 @@ public interface ArgumentResolver {
     result[index] = this.resolve(mappingDefinition, update, index);
   }
 
+  /**
+   * Retrieves required information for the handler parameter from the update.
+   * @param mappingDefinition The full information about handler method.
+   * @param update The incoming update (i.e. telegram message)
+   * @param index The index of the handler method parameter to resolve.
+   * @return
+   */
+  @Nullable
   default Object resolve(
     MappingDefinition mappingDefinition,
     Update update,
