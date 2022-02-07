f723e4dc3c8b1bd3b3352b82e10944459d590ad8
Vladimir Shefer <quameu@gmail.com>
2022-02-07 23:14:56 +0300

    PhotoSizeArgumentResolverImpl: Refactoring to new signature & Support single PhotoSize

--- spring-boot-starter-telegram/src/main/java/io/github/vladimirshefer/springbootstartertelegram/argument_resolvers/PhotoSizeArgumentResolverImpl.java
+++ spring-boot-starter-telegram/src/main/java/io/github/vladimirshefer/springbootstartertelegram/argument_resolvers/PhotoSizeArgumentResolverImpl.java
@@ -3,23 +3,78 @@ package io.github.vladimirshefer.springbootstartertelegram.argument_resolvers;
 import static io.github.vladimirshefer.springbootstartertelegram.telegram.util.UpdateUtil.getPhotoOrNull;
 
 import io.github.vladimirshefer.springbootstartertelegram.telegram.dto.MappingDefinition;
+import io.github.vladimirshefer.springbootstartertelegram.telegram.util.UpdateUtil;
+import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.objects.PhotoSize;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 @Component
 public class PhotoSizeArgumentResolverImpl implements ArgumentResolver {
 
+  /**
+   * If parameter is
+   * <pre>{@code List<PhotoSize>}</pre>
+   * then all PhotoSize-s will be returned as list.
+   *
+   * If parameter type is PhotoSize, then the biggest PhotoSize will be returned.
+   *
+   * If parameter has another type then null will be returned.
+   *
+   * If update has no PhotoSize-s then null will be returned.
+   *
+   * @param mappingDefinition The full information about handler method.
+   * @param update            The incoming update (i.e. telegram message)
+   * @param index             The index of the handler method parameter to resolve.
+   * @return List of Photo size or
+   */
   @Override
-  public void resolve(
+  public Object resolve(
     MappingDefinition mappingDefinition,
     Update update,
-    Object[] result,
-    int index) {
+    int index
+  ) {
     Type[] genericParameterTypes = mappingDefinition.getOriginalMethod().getGenericParameterTypes();
-    if (genericParameterTypes[index].getTypeName().contains("PhotoSize")) {
-      result[index] = getPhotoOrNull(update);
+
+    boolean isList = mappingDefinition.getOriginalMethod().getParameterTypes()[index].equals(
+      List.class);
+    if (isList) {
+      boolean hasGeneric = hasGeneric(genericParameterTypes[index], PhotoSize.class);
+      if (hasGeneric) {
+        return getPhotoOrNull(update);
+      }
     }
 
+    boolean isPhotoSize = mappingDefinition.getOriginalMethod()
+      .getParameterTypes()[index].equals(PhotoSize.class);
+
+    if (isPhotoSize) {
+      return getBiggestPhotoSize(update);
+    }
+
+    return null;
   }
+
+  private PhotoSize getBiggestPhotoSize(Update update) {
+    return UpdateUtil.getPhotoOrEmpty(update)
+      .stream()
+      .max(Comparator.comparing(PhotoSize::getHeight))
+      .orElse(null);
+  }
+
+  private boolean hasGeneric(Type parameterType, Class<?> genericType) {
+    if (parameterType instanceof ParameterizedType) {
+      ParameterizedType parameterizedType = (ParameterizedType) parameterType;
+      return Arrays.stream(parameterizedType.getActualTypeArguments())
+        .map(Type::getTypeName)
+        .anyMatch(it -> it.equals(genericType.getTypeName()));
+    }
+    return false;
+  }
+
 }
