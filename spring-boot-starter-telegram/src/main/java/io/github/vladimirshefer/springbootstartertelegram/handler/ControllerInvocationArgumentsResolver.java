95b6e50e980cf8e2bb64f964d8727d83a4756268
Vladimir Shefer <quameu@gmail.com>
2022-02-10 16:01:57 +0300

    Add javadoc for ControllerInvocationArgumentsResolver

--- spring-boot-starter-telegram/src/main/java/io/github/vladimirshefer/springbootstartertelegram/handler/ControllerInvocationArgumentsResolver.java
+++ spring-boot-starter-telegram/src/main/java/io/github/vladimirshefer/springbootstartertelegram/handler/ControllerInvocationArgumentsResolver.java
@@ -7,12 +7,28 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
+/**
+ * This class is used to extract arguments required by handler (controller method).
+ * The main source of arguments values is the Update from Telegram,
+ * but implementations are allowed use any other datasource (e.g. database, api, files)
+ * required for correct binding.
+ *
+ */
 @Component
 @RequiredArgsConstructor
 public class ControllerInvocationArgumentsResolver {
 
   private final List<ArgumentResolver> argumentResolvers;
 
+  /**
+   * This method collects the values for arguments of controller method.
+   * This method is called for each update for each controller method
+   * which was not filtered out by MethodFilter implementations.
+   *
+   * @param mappingDefinition the information about handler method
+   * @param update the incoming telegram update (message)
+   * @return The array of the parameters for calling handler method.
+   */
   public Object[] getArguments(
     MappingDefinition mappingDefinition,
     Update update
