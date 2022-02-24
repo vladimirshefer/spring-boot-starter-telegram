36c6fd7415352a14b7d23b2711293144ff0e83e5
Vladimir Shefer <quameu@gmail.com>
2022-02-25 00:22:09 +0300

    Refactoring: Rename ::resolve::mappingDefinition to ::...::method in PollArgumentResolverImpl
    This commit is a part of buch of commits and a part of global refactoring. Nothing is changed in behaviour. Please see the nearest commits in history.

--- spring-boot-starter-telegram/src/main/java/io/github/vladimirshefer/springbootstartertelegram/argument_resolvers/PollArgumentResolverImpl.java
+++ spring-boot-starter-telegram/src/main/java/io/github/vladimirshefer/springbootstartertelegram/argument_resolvers/PollArgumentResolverImpl.java
@@ -11,19 +11,19 @@ import org.telegram.telegrambots.meta.api.objects.polls.Poll;
 public class PollArgumentResolverImpl implements ArgumentResolver {
 
   public Object resolve(
-    MappingDefinition mappingDefinition,
+    MappingDefinition method,
     Update update,
     int index
   ) {
-    Class<?> parameterType = getParameterType(mappingDefinition, index);
+    Class<?> parameterType = getParameterType(method, index);
     if (parameterType.equals(Poll.class)) {
       return getPollOrNull(update);
     }
     return null;
   }
 
-  private Class<?> getParameterType(MappingDefinition mappingDefinition, int index) {
-    Class<?>[] parameterTypes = mappingDefinition.getOriginalMethod().getParameterTypes();
+  private Class<?> getParameterType(MappingDefinition method, int index) {
+    Class<?>[] parameterTypes = method.getOriginalMethod().getParameterTypes();
     return parameterTypes[index];
   }
 
