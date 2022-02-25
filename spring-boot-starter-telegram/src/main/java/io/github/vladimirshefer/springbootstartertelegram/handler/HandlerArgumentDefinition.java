39ddabca5ea8a9f328b2b67136fcf7de6263d2d1
Vladimir Shefer <quameu@gmail.com>
2022-02-25 03:28:53 +0300

    Bugfix: HandlerArgumentDefinition compilation error
    "Field might not be initialized"

--- spring-boot-starter-telegram/src/main/java/io/github/vladimirshefer/springbootstartertelegram/handler/HandlerArgumentDefinition.java
+++ spring-boot-starter-telegram/src/main/java/io/github/vladimirshefer/springbootstartertelegram/handler/HandlerArgumentDefinition.java
@@ -2,7 +2,6 @@ package io.github.vladimirshefer.springbootstartertelegram.handler;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
 import java.lang.annotation.Annotation;
@@ -11,18 +10,26 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@RequiredArgsConstructor
 @EqualsAndHashCode
 @ToString
 public class HandlerArgumentDefinition {
-  @Getter
+
   private final HandlerMethodDefinition handlerMethodDefinition;
-  @Getter
   private final int parameterIndex;
 
   @Getter
-  private final List<Class<? extends Annotation>> annotations =
-    Collections.unmodifiableList(
+  private final List<Class<? extends Annotation>> annotations;
+
+  @Getter
+  private final Class<?> type;
+
+  public HandlerArgumentDefinition(HandlerMethodDefinition handlerMethodDefinition, int parameterIndex) {
+    this.handlerMethodDefinition = handlerMethodDefinition;
+    this.parameterIndex = parameterIndex;
+    this.type = handlerMethodDefinition
+      .getOriginalMethod()
+      .getParameterTypes()[parameterIndex];
+    this.annotations = Collections.unmodifiableList(
       Arrays
         .stream(
           handlerMethodDefinition
@@ -32,11 +39,5 @@ public class HandlerArgumentDefinition {
         .map(Annotation::annotationType)
         .collect(Collectors.toList())
     );
-
-  @Getter
-  private final Class<?> type =
-    handlerMethodDefinition
-      .getOriginalMethod()
-      .getParameterTypes()[parameterIndex];
-
+  }
 }
