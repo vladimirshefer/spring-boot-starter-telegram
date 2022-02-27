172a4472215578cd3f95975debe46b2b6201df96
Vladimir Shefer <quameu@gmail.com>
2022-02-27 23:59:40 +0300

    Refactoring: Simplify TextBodyMethodFilterImpl

--- spring-boot-starter-telegram/src/main/java/io/github/vladimirshefer/springbootstartertelegram/method_filter/TextBodyMethodFilterImpl.java
+++ spring-boot-starter-telegram/src/main/java/io/github/vladimirshefer/springbootstartertelegram/method_filter/TextBodyMethodFilterImpl.java
@@ -1,49 +1,29 @@
 package io.github.vladimirshefer.springbootstartertelegram.method_filter;
 
-import io.github.vladimirshefer.springbootstartertelegram.handler.HandlerMethodDefinition;
+import io.github.vladimirshefer.springbootstartertelegram.handler.HandlerArgumentDefinition;
 import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
+import java.util.Optional;
+
+/**
+ * Filters out controller method if update has no text and method has String parameter without
+ * Nullable annotation.
+ */
 @Component
-public class TextBodyMethodFilterImpl implements MethodFilter {
+public class TextBodyMethodFilterImpl extends SimpleMethodFilter {
 
-  /**
-   * Filters out controller method if update mas no text and method has String parameter without
-   * Nullable annotation.
-   *
-   * @param update the telegram message
-   * @param method the controller method information
-   * @return false if this update should not be handled by this method. True if you don't mind.
-   */
   @Override
-  public boolean isMatch(Update update, HandlerMethodDefinition method) {
-    return getMessageText(update) != null || !isHasRequiredStringParameter(method);
+  public boolean updateHasValue(Update update) {
+    return Optional.ofNullable(update).map(Update::getMessage).map(Message::getText).isPresent();
   }
 
-  private boolean isHasRequiredStringParameter(HandlerMethodDefinition method) {
-    boolean hasRequiredStringParameter = false;
-
-    for (int i = 0; i < getParametersCount(method); i++) {
-      if (isRequiredString(method, i)) {
-        hasRequiredStringParameter = true;
-        break;
-      }
-    }
-    return hasRequiredStringParameter;
-  }
-
-  private String getMessageText(Update update) {
-    return update.getMessage() != null ? update.getMessage().getText() : null;
-  }
-
-  private int getParametersCount(HandlerMethodDefinition method) {
-    return method.getTargetMethod().getParameters().length;
-  }
-
-  private boolean isRequiredString(HandlerMethodDefinition method, int parameterIndex) {
-    boolean isString = method.getArgument(parameterIndex).getType().equals(String.class);
-    boolean isRequired = !method.getArgument(parameterIndex).hasAnnotation("Nullable");
-    return isString && isRequired;
+  @Override
+  public boolean valueIsRequired(HandlerArgumentDefinition argument) {
+    return !argument.getAnnotations().isEmpty() &&
+      argument.getType().equals(String.class) &&
+      !argument.hasAnnotation("Nullable");
   }
 
 }
