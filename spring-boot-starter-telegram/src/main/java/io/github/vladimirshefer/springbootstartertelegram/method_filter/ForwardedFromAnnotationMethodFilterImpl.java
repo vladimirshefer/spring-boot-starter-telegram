77707c468ed24676138edc4c1ae5f2f629bfd2f8
Vladimir Shefer <quameu@gmail.com>
2022-02-28 00:00:46 +0300

    Refactoring: Simplify ForwardedFromAnnotationMethodFilterImpl

--- spring-boot-starter-telegram/src/main/java/io/github/vladimirshefer/springbootstartertelegram/method_filter/ForwardedFromAnnotationMethodFilterImpl.java
+++ spring-boot-starter-telegram/src/main/java/io/github/vladimirshefer/springbootstartertelegram/method_filter/ForwardedFromAnnotationMethodFilterImpl.java
@@ -2,7 +2,6 @@ package io.github.vladimirshefer.springbootstartertelegram.method_filter;
 
 import io.github.vladimirshefer.springbootstartertelegram.annotations.ForwardedFrom;
 import io.github.vladimirshefer.springbootstartertelegram.handler.HandlerArgumentDefinition;
-import io.github.vladimirshefer.springbootstartertelegram.handler.HandlerMethodDefinition;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
@@ -10,25 +9,20 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import java.util.Optional;
 
 @Component
-public class ForwardedFromAnnotationMethodFilterImpl implements MethodFilter {
+public class ForwardedFromAnnotationMethodFilterImpl extends SimpleMethodFilter {
 
   @Override
-  public boolean isMatch(Update update, HandlerMethodDefinition method) {
-    boolean hasForwardedFrom = Optional.ofNullable(update)
+  public boolean valueIsRequired(HandlerArgumentDefinition argument) {
+    return argument.hasAnnotation(ForwardedFrom.class) &&
+      !argument.hasAnnotation("Nullable");
+  }
+
+  @Override
+  public boolean updateHasValue(Update update) {
+    return Optional.ofNullable(update)
       .map(Update::getMessage)
       .map(Message::getForwardFrom)
       .isPresent();
-
-    for (HandlerArgumentDefinition argument : method.getArguments()) {
-      boolean argumentMatched = !argument.hasAnnotation(ForwardedFrom.class) ||
-        argument.hasAnnotation("Nullable") || hasForwardedFrom;
-
-      if (!argumentMatched) {
-        return false;
-      }
-    }
-
-    return true;
   }
 
 }
