047bab3be001ceccf1fdba9b95142c7c16204c86
Vladimir Shefer <quameu@gmail.com>
2022-02-27 23:59:59 +0300

    Refactoring: Simplify ReplyToMessageAnnotationMethodFilterImpl

--- spring-boot-starter-telegram/src/main/java/io/github/vladimirshefer/springbootstartertelegram/method_filter/ReplyToMessageAnnotationMethodFilterImpl.java
+++ spring-boot-starter-telegram/src/main/java/io/github/vladimirshefer/springbootstartertelegram/method_filter/ReplyToMessageAnnotationMethodFilterImpl.java
@@ -2,7 +2,6 @@ package io.github.vladimirshefer.springbootstartertelegram.method_filter;
 
 import io.github.vladimirshefer.springbootstartertelegram.annotations.ReplyToMessage;
 import io.github.vladimirshefer.springbootstartertelegram.handler.HandlerArgumentDefinition;
-import io.github.vladimirshefer.springbootstartertelegram.handler.HandlerMethodDefinition;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
@@ -10,25 +9,18 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import java.util.Optional;
 
 @Component
-public class ReplyToMessageAnnotationMethodFilterImpl implements MethodFilter {
+public class ReplyToMessageAnnotationMethodFilterImpl extends SimpleMethodFilter {
 
   @Override
-  public boolean isMatch(Update update, HandlerMethodDefinition method) {
-    boolean hasReplyToMessage = Optional.ofNullable(update)
+  public boolean updateHasValue(Update update) {
+    return Optional.ofNullable(update)
       .map(Update::getMessage)
       .map(Message::getReplyToMessage)
       .isPresent();
+  }
 
-    for (HandlerArgumentDefinition argument : method.getArguments()) {
-      boolean argumentMatched = !argument.hasAnnotation(ReplyToMessage.class) ||
-        argument.hasAnnotation("Nullable") || hasReplyToMessage;
-
-      if (!argumentMatched) {
-        return false;
-      }
-    }
-
-    return true;
+  public boolean valueIsRequired(HandlerArgumentDefinition argument) {
+    return argument.hasAnnotation(ReplyToMessage.class) && !argument.hasAnnotation("Nullable");
   }
 
 }
