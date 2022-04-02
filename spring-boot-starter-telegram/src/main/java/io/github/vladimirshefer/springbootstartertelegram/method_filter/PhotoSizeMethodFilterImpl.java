fd48890358f39276d3ccd3e9f7232a1f9d3b76a0
Vladimir Shefer <quameu@gmail.com>
2022-04-02 14:32:50 +0300

    PhotoSize method filter refactoring

--- spring-boot-starter-telegram/src/main/java/io/github/vladimirshefer/springbootstartertelegram/method_filter/PhotoSizeMethodFilterImpl.java
+++ spring-boot-starter-telegram/src/main/java/io/github/vladimirshefer/springbootstartertelegram/method_filter/PhotoSizeMethodFilterImpl.java
@@ -1,12 +1,13 @@
 package io.github.vladimirshefer.springbootstartertelegram.method_filter;
 
+import io.github.vladimirshefer.springbootstartertelegram.handler.HandlerArgumentDefinition;
 import io.github.vladimirshefer.springbootstartertelegram.handler.HandlerMethodDefinition;
-import java.util.List;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.PhotoSize;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
-import java.util.Arrays;
+import java.lang.reflect.Type;
+import java.util.List;
 
 @Component
 public class PhotoSizeMethodFilterImpl implements MethodFilter {
@@ -20,8 +21,10 @@ public class PhotoSizeMethodFilterImpl implements MethodFilter {
   }
 
   private boolean hasPhotoSizeListParameter(HandlerMethodDefinition method) {
-    return Arrays
-      .stream(method.getOriginalMethod().getGenericParameterTypes())
-      .anyMatch(p -> p.getTypeName().contains("PhotoSize"));
+    return method.getArguments().stream()
+      .map(HandlerArgumentDefinition::getGenericType)
+      .map(Type::getTypeName)
+      .anyMatch(it -> it.contains(PhotoSize.class.getSimpleName()));
   }
+
 }
