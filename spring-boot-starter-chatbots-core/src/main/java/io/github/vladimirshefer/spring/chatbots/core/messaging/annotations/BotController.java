2fafa73d9a9fbf9bd4fb5a3d72431913d41342db
Vladimir Shefer <quameu@gmail.com>
2022-11-04 01:36:39 +0400

    Allow specify messengers for controller

--- spring-boot-starter-chatbots-core/src/main/java/io/github/vladimirshefer/spring/chatbots/core/messaging/annotations/BotController.java
+++ spring-boot-starter-chatbots-core/src/main/java/io/github/vladimirshefer/spring/chatbots/core/messaging/annotations/BotController.java
@@ -2,13 +2,17 @@ package io.github.vladimirshefer.spring.chatbots.core.messaging.annotations;
 
 import org.springframework.stereotype.Component;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Component
+@Inherited
 public @interface BotController {
+  /**
+   * Snake case names of messengers which are handled by this controller.
+   * Examples: "telegram", "discord", "slack".
+   * If empty array, then controller is considered for all messengers.
+   */
+  public String[] messengers() default {};
 }
--- spring-boot-starter-chatbots/src/main/java/io/github/vladimirshefer/spring/chatbots/annotations/TelegramController.java
+++ spring-boot-starter-chatbots/src/main/java/io/github/vladimirshefer/spring/chatbots/annotations/TelegramController.java
@@ -4,11 +4,14 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+
+import io.github.vladimirshefer.spring.chatbots.core.messaging.annotations.BotController;
 import org.springframework.stereotype.Component;
 
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Component
+@BotController(messengers = {"telegram"})
 public @interface TelegramController {
 
 }
