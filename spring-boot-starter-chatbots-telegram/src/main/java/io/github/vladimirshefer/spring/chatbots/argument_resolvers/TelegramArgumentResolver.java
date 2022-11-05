c52d8f1e61b6ec5d03412cc2b7ea5fbd8a18e3be
Vladimir Shefer <quameu@gmail.com>
2022-11-05 18:15:41 +0400

    Fix TelegramArgumentResolver

--- spring-boot-starter-chatbots-telegram/src/main/java/io/github/vladimirshefer/spring/chatbots/argument_resolvers/TelegramArgumentResolver.java
+++ spring-boot-starter-chatbots-telegram/src/main/java/io/github/vladimirshefer/spring/chatbots/argument_resolvers/TelegramArgumentResolver.java
@@ -2,12 +2,13 @@ package io.github.vladimirshefer.spring.chatbots.argument_resolvers;
 
 import io.github.vladimirshefer.spring.chatbots.core.facade.EventFacade;
 import io.github.vladimirshefer.spring.chatbots.core.handler.HandlerArgumentDefinition;
+import io.github.vladimirshefer.spring.chatbots.core.messaging.ArgumentResolver;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 /**
  * Class to set arguments when calling update handler method in bot controller.
  */
-public interface TelegramArgumentResolver {
+public interface TelegramArgumentResolver extends ArgumentResolver {
 
   default boolean shouldResolve(HandlerArgumentDefinition argumentDefinition) {
     return true;
