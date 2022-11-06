f64cb03feb091bd1c694274e2ea3a705565a5b2b
Vladimir Shefer <quameu@gmail.com>
2022-11-07 01:13:27 +0400

    Define core components in spring factories

---
+++ spring-chatbots-core/src/main/java/io/github/vladimirshefer/spring/chatbots/core/SpringChatbotsCoreConfiguration.java
@@ -0,0 +1,9 @@
+package io.github.vladimirshefer.spring.chatbots.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class SpringChatbotsCoreConfiguration {
+}
