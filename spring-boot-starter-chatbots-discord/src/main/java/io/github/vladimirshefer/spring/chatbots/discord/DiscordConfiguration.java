5a751a3a6a94b0ee6f4e4799f9e586a13350ebeb
Vladimir Shefer <quameu@gmail.com>
2022-11-08 03:06:38 +0400

    Reimplement DiscordConfiguration

--- spring-boot-starter-chatbots-discord/src/main/java/io/github/vladimirshefer/spring/chatbots/discord/DiscordConfiguration.java
+++ spring-boot-starter-chatbots-discord/src/main/java/io/github/vladimirshefer/spring/chatbots/discord/DiscordConfiguration.java
@@ -5,30 +5,41 @@ import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import io.github.vladimirshefer.spring.chatbots.core.engine.EventListener;
 import io.github.vladimirshefer.spring.chatbots.discord.facade.DiscordMessageCreateEventFacade;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
+@ComponentScan
+@Slf4j
 public class DiscordConfiguration {
 
-  @Value("${spring.chatbots.discord.bot.token:}")
-  String token;
-
   @Bean
-  public GatewayDiscordClient discordBot(
+  public GatewayDiscordClient gatewayDiscordClient(
+    @Value("${spring.chatbots.discord.bot.token:}")
+    String token,
     EventListener eventListener
   ) {
-    if (token.isEmpty()) {
-      return null;
-    }
-    final DiscordClient client = DiscordClient.create(token);
-    final GatewayDiscordClient gateway = client.login().block();
+    GatewayDiscordClient gateway = DiscordClient.create(token)
+      .gateway()
+      .login()
+      .block();
 
     gateway.on(MessageCreateEvent.class)
-      .subscribe(event -> eventListener.handleMessage(new DiscordMessageCreateEventFacade(event)));
+      .subscribe(event -> {
+        try {
+          List<Object> results = eventListener.handleMessage(new DiscordMessageCreateEventFacade(event));
 
-    gateway.onDisconnect().block();
+        } catch (Throwable throwable) {
+          log.error("Could not handle event", throwable);
+        }
+      });
+
+//    gateway.getChannelById(Snowflake.of())
 
     return gateway;
   }
